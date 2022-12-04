f3bd88b0d863e2082d73af20e895aa02c3176415
cypherhan <clafrncsc@gmail.com>
2022-12-04 14:55:07 +0800

    header button format

--- registration.php
+++ registration.php
@@ -19,7 +19,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/registration.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
+    <title>KADS | Register</title>
     <!-- css -->    
     <link rel="stylesheet" href="css/footer.css" />
     

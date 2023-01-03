2491276de1d34639224469476c7591c2f3b8e1c1
Alvy Depositar <depalvy@gmail.com>
2023-01-03 08:52:17 +0800

    sql file updated

--- SQL db/kads.sql
+++ SQL db/kads.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2022 at 11:14 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Jan 03, 2023 at 01:51 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,116 +24,193 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customer_account`
+-- Table structure for table `categories`
 --
 
-CREATE TABLE `customer_account` (
-  `ID` int(11) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `customer_detail_ID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `categoryName` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `categoryName`) VALUES
+(1, 'Sushi Rolls'),
+(2, 'Bento Box'),
+(3, 'Temaki Wraps');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customer_address`
+-- Table structure for table `info_accts`
 --
 
-CREATE TABLE `customer_address` (
-  `ID` int(11) NOT NULL,
-  `housenum` int(11) DEFAULT NULL,
-  `city` varchar(255) DEFAULT NULL,
-  `state` varchar(255) DEFAULT NULL,
-  `zip` int(11) DEFAULT NULL,
-  `addPhone` int(11) DEFAULT NULL,
-  `customer_detail_ID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `customer_detail`
---
-
-CREATE TABLE `customer_detail` (
-  `ID` int(11) NOT NULL,
+CREATE TABLE `info_accts` (
+  `id` int(11) NOT NULL,
   `firstname` varchar(255) DEFAULT NULL,
   `lastname` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `phone` int(11) DEFAULT NULL,
-  `birthdate` date DEFAULT NULL,
-  `customer_address_ID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `birthday` date DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `info_accts`
+--
+
+INSERT INTO `info_accts` (`id`, `firstname`, `lastname`, `birthday`, `username`, `phone`, `password`, `role`) VALUES
+(12, 'Admin', 'Admin', '2001-08-13', 'admin', '09123456789', '$2y$10$HkVmVdmQHxyQE0ARqbPej.pmBGopjlUjj/ZQpFYLH6mFWwPn2tJ0G', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `image` varchar(400) DEFAULT NULL,
+  `productName` varchar(255) DEFAULT NULL,
+  `price` int(11) NOT NULL,
+  `category` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `image`, `productName`, `price`, `category`) VALUES
+(7, 'IMG-63b35088b935b4.04949481.png', 'Cucumber Roll', 55, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_address`
+--
+
+CREATE TABLE `user_address` (
+  `id` int(11) NOT NULL,
+  `house` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `province` varchar(255) DEFAULT NULL,
+  `contactNum` int(11) DEFAULT NULL,
+  `info` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_orders`
+--
+
+CREATE TABLE `user_orders` (
+  `id` int(11) NOT NULL,
+  `orders` int(255) NOT NULL,
+  `orderTotal` int(11) NOT NULL,
+  `timeOrder` time DEFAULT NULL,
+  `dateOrder` date DEFAULT NULL,
+  `statusOrder` varchar(255) DEFAULT NULL,
+  `user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `customer_account`
+-- Indexes for table `categories`
 --
-ALTER TABLE `customer_account`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `customer_detail_ID` (`customer_detail_ID`);
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `customer_address`
+-- Indexes for table `info_accts`
 --
-ALTER TABLE `customer_address`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `customer_detail_ID` (`customer_detail_ID`);
+ALTER TABLE `info_accts`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `customer_detail`
+-- Indexes for table `products`
 --
-ALTER TABLE `customer_detail`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `customer_address_ID` (`customer_address_ID`);
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `products_ibfk` (`category`);
+
+--
+-- Indexes for table `user_address`
+--
+ALTER TABLE `user_address`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_address_ibfk` (`info`);
+
+--
+-- Indexes for table `user_orders`
+--
+ALTER TABLE `user_orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_orders_ibfk` (`user`),
+  ADD KEY `orders_ibfk` (`orders`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `customer_account`
+-- AUTO_INCREMENT for table `categories`
 --
-ALTER TABLE `customer_account`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `customer_address`
+-- AUTO_INCREMENT for table `info_accts`
 --
-ALTER TABLE `customer_address`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `info_accts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT for table `customer_detail`
+-- AUTO_INCREMENT for table `products`
 --
-ALTER TABLE `customer_detail`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user_address`
+--
+ALTER TABLE `user_address`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_orders`
+--
+ALTER TABLE `user_orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `customer_account`
+-- Constraints for table `products`
 --
-ALTER TABLE `customer_account`
-  ADD CONSTRAINT `customer_account_ibfk_1` FOREIGN KEY (`customer_detail_ID`) REFERENCES `customer_detail` (`ID`);
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk` FOREIGN KEY (`category`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `customer_address`
+-- Constraints for table `user_address`
 --
-ALTER TABLE `customer_address`
-  ADD CONSTRAINT `customer_address_ibfk_1` FOREIGN KEY (`customer_detail_ID`) REFERENCES `customer_detail` (`ID`);
+ALTER TABLE `user_address`
+  ADD CONSTRAINT `user_address_ibfk` FOREIGN KEY (`info`) REFERENCES `info_accts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `customer_detail`
+-- Constraints for table `user_orders`
 --
-ALTER TABLE `customer_detail`
-  ADD CONSTRAINT `customer_detail_ibfk_1` FOREIGN KEY (`customer_address_ID`) REFERENCES `customer_address` (`ID`);
+ALTER TABLE `user_orders`
+  ADD CONSTRAINT `orders_ibfk` FOREIGN KEY (`orders`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_orders_ibfk` FOREIGN KEY (`user`) REFERENCES `info_accts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

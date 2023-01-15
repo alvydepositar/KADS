af485ad5dd44ff130d8563cdc6ce07f87f80918f
cypherhan <clafrncsc@gmail.com>
2023-01-15 22:24:00 +0800

    adjusted menu "add to cart" text btn

--- menu.php
+++ menu.php
@@ -164,7 +164,8 @@ if (!isset($_SESSION["loggedin"]) && !isset($_SESSION['role'])){
                         <a href="menu.php?p_id=<?php echo $row['p_id']; ?>&
                         p_image=<?php echo $row['image']; ?>&
                         p_price=<?php echo $row['price']; ?>&
-                        p_name=<?php echo $row['productName']; ?>"> Add to Cart </a>                    
+                        p_name=<?php echo $row['productName']; ?>"
+                        class="btnAddToCart"> Add to Cart </a>                    
                 </div>
             </div>
             

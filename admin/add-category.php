23a15a9b6cfb9eaa94a537c0eb967cdeb8a41b53
Alvy Depositar <depalvy@gmail.com>
2023-01-17 12:08:11 +0800

    update

--- admin/add-category.php
+++ admin/add-category.php
@@ -4,7 +4,7 @@ session_start();
 // check if user is logged in
 if (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin']) {
     //user is not logged in, redirect to login page
-    header("Location: login.php");
+    header("Location: ../login.php");
     exit();
 }
 
